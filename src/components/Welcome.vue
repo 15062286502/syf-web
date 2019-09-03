1531e4651d5f89761f2eead264e0d30e9dcaf976
songyufeng <songyufeng@glaway.com>
2019-09-03 14:24:19 +0800

    9.3container布局初步优化

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,9 +1,16 @@
 <template>
-  <el-container>
-    <el-header style="height: 80px"></el-header>
-    <el-main>
-      <el-container style="height: 600px;  border: 1px solid #eee">
-        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-container style="height: 600px; border: 1px solid #eee">
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>{{user}}</span>
+        </el-header>
+        <el-container>
+        <el-aside width="200px">
           <el-menu :default-openeds="['1', '3']">
             <el-submenu index="1">
               <template slot="title"><i class="el-icon-message"></i>导航一</template>
@@ -28,17 +35,6 @@
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-container>
-          <el-header style="text-align: right; font-size: 12px">
-            <el-dropdown>
-              <i class="el-icon-setting" style="margin-right: 15px"></i>
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>查看</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-            <span>{{user}}</span>
-          </el-header>
-
           <el-main>
             <el-table :data="tableData">
               <el-table-column prop="date" label="日期" width="140">
@@ -51,8 +47,6 @@
           </el-main>
         </el-container>
       </el-container>
-    </el-main>
-  </el-container>
 </template>
 
 <script>
@@ -78,5 +72,10 @@ export default {
 
   .el-aside {
     color: #333;
+  }
+  .el-main {
+    color: #333;
+    text-align: center;
+    line-height: 160px;
   }
 </style>
