5c136d10e78e06d8a233d2a61287f2fca9df699b
syf <15062286502@163.com>
2019-09-03 22:43:05 +0800

    9.3最终提交

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
-      <el-container style="height: 600px; border: 1px solid #eee">
-        <el-header style="text-align: right; font-size: 12px">
+      <el-container style="height: 700px; border: 1px solid #eee">
+        <el-header>
           <el-dropdown>
             <i class="el-icon-setting" style="margin-right: 15px"></i>
             <el-dropdown-menu slot="dropdown">
@@ -10,7 +10,7 @@
           <span>{{user}}</span>
         </el-header>
         <el-container>
-        <el-aside width="200px">
+        <el-aside>
           <el-menu :default-openeds="['1', '3']" v-for="m in menulist" :key="m">
             <el-submenu index="m.id">
               <template slot="title"><i class="el-icon-message"></i>{{m.name}}</template>
@@ -31,6 +31,9 @@
             </el-table>
           </el-main>
         </el-container>
+        <el-footer>
+          <span>Copyright © 2019 - 2019 . All Rights Reserved.南航syf版权所有</span>
+        </el-footer>
       </el-container>
 </template>
 
@@ -63,14 +66,26 @@ export default {
     background-color: #B3C0D1;
     color: #333;
     line-height: 60px;
+    text-align: right;
+    font-size: 12px
   }
 
   .el-aside {
+    background-color: #cac6c6;
     color: #333;
   }
   .el-main {
     color: #333;
     text-align: center;
     line-height: 160px;
+  }
+  .el-footer{
+    text-align: center;
+  }
+  .el-menu{
+    background-color:#cac6c6;
+  }
+  .el-menu-item-group{
+    background-color: #cac6c6;
   }
 </style>
