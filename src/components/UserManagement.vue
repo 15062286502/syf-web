1d177bc44071ac19cbc1ad94b0d39f12045ee3ec
syf <15062286502@163.com>
2019-10-15 22:59:02 +0800

    10.15用户添加初步实现，较为粗糙，没有做校验

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -76,9 +76,9 @@
           layout="total, sizes, prev, pager, next, jumper"
           :total=this.total>
         </el-pagination>
-        <el-dialog title="新增用户" :visible.sync="dialogFormVisible" width="40%">
-          <el-form v-loading="loading" :model="userNew">
-            <el-form-item label="用户名" :label-width="formLabelWidth">
+        <el-dialog title="新增用户" :visible.sync="dialogFormVisible" width="30%">
+          <el-form v-loading="loading" :model="userNew" :rules="add_rules">
+            <el-form-item label="用户名" :label-width="formLabelWidth" prop="name">
               <el-input auto-complete="off" v-model="userNew.name"></el-input>
             </el-form-item>
             <el-form-item label="密码" prop="password" :label-width="formLabelWidth">
@@ -89,20 +89,20 @@
               <el-input type="password" v-model="userNew.passwordAgain"
                         auto-complete="off" placeholder="确认密码"></el-input>
             </el-form-item>
-            <el-form-item label="角色" :label-width="formLabelWidth">
+            <el-form-item label="角色" prop="role" :label-width="formLabelWidth" >
               <el-select  placeholder="请选择角色" v-model="userNew.role">
                 <el-option label="店长" value="店长"></el-option>
                 <el-option label="骑手" value="骑手"></el-option>
                 <el-option label="服务员" value="服务员"></el-option>
               </el-select>
             </el-form-item>
-            <el-form-item label="真实姓名" :label-width="formLabelWidth">
+            <el-form-item label="真实姓名" :label-width="formLabelWidth" prop="realName">
               <el-input auto-complete="off" v-model="userNew.realName"></el-input>
             </el-form-item>
-            <el-form-item label="地址" :label-width="formLabelWidth">
+            <el-form-item label="地址" :label-width="formLabelWidth" prop="address">
               <el-input auto-complete="off" v-model="userNew.address"></el-input>
             </el-form-item>
-            <el-form-item label="联系方式" :label-width="formLabelWidth">
+            <el-form-item label="联系方式" :label-width="formLabelWidth" prop="phone">
               <el-input auto-complete="off" v-model="userNew.phone"></el-input>
             </el-form-item>
           </el-form>
@@ -141,7 +141,15 @@ export default {
         phone: null
       },
       total: 0,
-      formLabelWidth: '80px'
+      formLabelWidth: '80px',
+      add_rules: {
+        name: [{required: true, message: '请输入正确的用户名', trigger: 'blur'}],
+        password: [{required: true, message: '请输入正确的密码', trigger: 'blur'}],
+        realName: [{required: true, message: '请输入正确的真实姓名', trigger: 'blur'}],
+        role: [{required: true, message: '请选择角色', trigger: 'blur'}],
+        address: [{required: true, message: '请输入正确的地址', trigger: 'blur'}],
+        phone: [{required: true, message: '请输入正确的手机号', trigger: 'blur'}]
+      }
     }
   },
   created () {
@@ -243,6 +251,10 @@ export default {
         this.$message({
           message: '添加成功',
           type: 'success'
+        })
+        this.getTable({
+          'pageInfo': this.pageInfo,
+          'loginName': this.loginName
         })
       }
       ).catch(res => {
